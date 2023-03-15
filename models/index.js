730c54b1597d0c6dacda1d2bac78dc500a8700de
Xiaojia <annacl1822@gmail.com>
2023-03-15 00:29:09 -0700

    add mostly functional api routes

--- models/index.js
+++ models/index.js
@@ -19,14 +19,15 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tag'
+  as: 'product_tags'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false    
-  }
+  }, 
+  as: 'tagged_products'
 })
 
 module.exports = {
